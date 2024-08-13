5dd97d83f103370c9a845cd1d6c15e80d7d8090e
SHussaing <husain187@hotmail.com>
2024-08-13 08:29:46 +0300

    added login component

--- HomeHuntFrontend/src/app/app.routes.ts
+++ HomeHuntFrontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ListingListComponent } from './components/listing-list/listing-list.component';
+import { LoginComponent } from './components/login/login.component';
+export const routes: Routes = [
+    {
+        path: '',
+        component: ListingListComponent,
+        title: 'Home Hunt | Listings'
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Home Hunt | Login'
+    }
+];
